0413293a22f5b8a287d5fd1d1976e3ab68955654
gucao <280780363@qq.com>
2018-06-10 09:09:11 +0800

    ...

--- signalr.demo/Demo.Identity/Data/SeedData.cs
+++ signalr.demo/Demo.Identity/Data/SeedData.cs
@@ -6,7 +6,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
-namespace Demo.Identity.Providers
+namespace Demo.Identity.Data
 {
     public class SeedData
     {
